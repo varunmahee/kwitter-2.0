0acc5f41f5a6fe6886a8640e8bda5c428785f4b5
varunmahee <84283092+varunmahee@users.noreply.github.com>
2021-11-07 15:23:10 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgeJ6s7LBqW6WtePpK0ObqBY0Fu8iDeK0",
+    authDomain: "kwitter-c236e.firebaseapp.com",
+    databaseURL: "https://kwitter-c236e-default-rtdb.firebaseio.com",
+    projectId: "kwitter-c236e",
+    storageBucket: "kwitter-c236e.appspot.com",
+    messagingSenderId: "417585304866",
+    appId: "1:417585304866:web:a23a3c4f924bd38448c17b"
+  };
+
+  // Initialize Firebase
+
